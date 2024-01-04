898f71c70559bda594d0fc4b7c97e69d672d9cd1
siwar142001 <72788299+siwar142001@users.noreply.github.com>
2024-01-04 08:56:34 +0100

    Update Classe.py

--- tp6-B/Classe.py
+++ tp6-B/Classe.py
@@ -1,6 +1,14 @@
 class Classe :
-    def __init(self,nb):
-        self.nb=nb  #nombre de personne 
+    def __init__(self,eleve,professeur): 
+        self.eleves=[eleve]
+        self.prof=professeur
+        
+    def add_eleves(self,e):
+        self.eleves.append(e)
+        
+    def __str__(self):
+        return "cette classe est composée par le prof {} et le nombre d'eleves est {} ".format(self.prof,len(self.eleves))
+        
 
 class Dossier :
     def __init__(self,nom,prenom,age,adresse,etat_civil,n_tel):
@@ -17,16 +25,16 @@ class Dossier :
 
      
 class Professeur:
-    def __init__(self, dossier , role="professeur"):
-        self.dossier = dossier
+    def __init__(self ,nom,prenom,age,adresse,etat_civil,n_tel, role="professeur"):
+        self.dossier = Dossier(nom,prenom,age,adresse,etat_civil,n_tel)
         self.role = role
     def __str__(self):
         ch=Dossier.__str__(self.dossier)+" " +str(self.role)
         return ch
           
 class Eleve:
-    def __init__(self,dossier,matricule,role="eleve"):
-        self.dossier = dossier
+    def __init__(self,nom,prenom,age,adresse,etat_civil,n_tel,matricule,role="eleve"):
+        self.dossier = Dossier(nom,prenom,age,adresse,etat_civil,n_tel)
         self.matricule=matricule 
         self.role = role
         
@@ -34,11 +42,12 @@ class Eleve:
         ch=Dossier.__str__(self.dossier)+" " +str(self.matricule)+" " +str(self.role)
         return ch
         
-dossier1=Dossier("Salmi","Siwar",22,"Kraainem","celib",4234567)
-#print(dossier1)
-dossier2=Dossier("MR","leProf",40,"Wavre","celib",999999)
-#dossier2=Dossier("Monsieur","Le prof",40,"Wavre","Marié",2563658)
-e1=Eleve(dossier1,"HE201969")
-p1=Professeur(dossier2)
-print(e1)
-print(p1)
+
+eleve1=Eleve("Salmi","Siwar",22,"Kraainem","celib",4234567,"HE201969")
+prof1=Professeur("MR","leProf",40,"Wavre","celib",999999)
+eleve2=Eleve("Titi","Tata",24,"LLN","celib",222222,"HE201960")
+classe1=Classe(eleve1,prof1)
+classe1.add_eleves(eleve2)
+print(classe1)
+print(eleve1)
+print(prof1)
